e857650dee1c9f47649995c07c728208167dc766
hasstMac <hasstmac@hasstMacs-MacBook-Air.local>
2018-04-10 19:33:41 -0700

    initial commit

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+// Local Javascript/jQuery //
+$(document).ready(function(){
+  // Initialize collapse button
+  $(".button-collapse").sideNav();
+  // Initialize collapse item
+  $('.collapsible').collapsible();
+  // Initialize carousel for flickr items
+  $('.carousel').carousel();
+  $('#flickr-carousel.carousel').carousel();
+  // Initialize full carousel for projects
+  // $('.carousel.carousel-slider').carousel({fullWidth: true});
+  // Initialize parallax
+  $('.parallax').parallax();
+
+});
